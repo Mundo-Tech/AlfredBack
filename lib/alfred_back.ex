b7e2be5321bbe799368de1a60a922c8c6bfbd1ac
jessica franca <jealunaifsp@gmail.com>
2022-12-11 00:25:13 -0300

    Import and configure route for reading user data

--- lib/alfred_back.ex
+++ lib/alfred_back.ex
@@ -3,4 +3,5 @@ defmodule AlfredBack do
 
   defdelegate create_user(params), to: User.Create, as: :call
   defdelegate delete_user(params), to: User.Delete, as: :call
+  defdelegate fetch_user(params), to: User.Get, as: :call
 end
---
+++ lib/alfred_back/installments_basis/schemas/user/get.ex
@@ -0,0 +1,19 @@
+defmodule AlfredBack.InstallmentsBasis.Schemas.User.Get do
+  alias Ecto.UUID
+  alias AlfredBack.InstallmentsBasis.Schemas.User
+  alias AlfredBack.Repo
+
+  def call(id) do
+    case UUID.cast(id) do
+      :error -> {:error, "Invaled ID format!"}
+      {:ok, uuid} -> get(uuid)
+    end
+  end
+
+  defp get(uuid) do
+    case Repo.get(User, uuid) do
+      nil -> {:error, "User not found!"}
+      user -> {:ok, user}
+    end
+  end
+end
